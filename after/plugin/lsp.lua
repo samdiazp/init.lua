b6e34e27566bc57d24ed637d90b3ffd8645e0170
piedrahitapablo <piedra@meltstudio.co>
2023-02-13 16:31:02 -0500

    add esc remap to abort completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -46,7 +46,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<Esc>'] = cmp.mapping.close(),
     })
 
 cmp_mappings['<Tab>'] = nil
