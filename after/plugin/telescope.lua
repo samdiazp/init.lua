c90b2066f38c834c80da5d57bb24c457ca6a0af8
piedrahitapablo <piedra@meltstudio.co>
2023-02-18 08:06:37 -0500

    improve telescope with new remaps and config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,53 @@
-local status, builtin = pcall(require,'telescope.builtin')
+local status, telescope = pcall(require, 'telescope')
 if (not status) then
-	return
+    print('telescope is not installed')
+    return
 end
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+local status, builtin = pcall(require, 'telescope.builtin')
+if (not status) then
+    return
+end
 
+telescope.setup({
+    defaults = {
+        layout_config = {
+            prompt_position = 'top',
+        },
+        prompt_prefix = '  ',
+        sorting_strategy = 'ascending',
+    },
+    pickers = {
+        find_files = {
+            find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
+        },
+        live_grep = {
+            additional_args = function()
+                return { "--hidden", '-g', '!.git' }
+            end
+        },
+        git_status = {
+            git_icons = {
+                added = "A",
+                changed = "M",
+                copied = "C",
+                deleted = "D",
+                renamed = "R",
+                unmerged = "U",
+                untracked = "?",
+            },
+        }
+    }
+})
+
+vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ts', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>tc', builtin.commands, {})
+vim.keymap.set('n', '<leader>tr', builtin.resume, {})
+
+vim.keymap.set('n', '<leader>tgf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tgc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>tgb', builtin.git_bcommits, {})
+vim.keymap.set('n', '<leader>tgs', builtin.git_status, {})
