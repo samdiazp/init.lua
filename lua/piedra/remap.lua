35b8cd4fd3db07cb914583dc3ade8a673d10674a
piedrahitapablo <piedra@meltstudio.co>
2023-03-06 09:25:54 -0500

    add some remaps for movement and search

--- lua/piedra/remap.lua
+++ lua/piedra/remap.lua
@@ -2,7 +2,20 @@ vim.g.mapleader = " "
 
 -- make home key go to the first non-empty char
 vim.keymap.set("n", "<Home>", "^")
+vim.keymap.set("v", "<Home>", "^")
 vim.keymap.set("i", "<Home>", "<C-o>^")
+
+-- remap meta+arrow to move words
+vim.keymap.set("n", "<M-b>", "b")
+vim.keymap.set("v", "<M-b>", "b")
+vim.keymap.set("i", "<M-b>", "<C-o>b")
+vim.keymap.set("n", "<M-f>", "w")
+vim.keymap.set("v", "<M-f>", "w")
+vim.keymap.set("i", "<M-f>", "<C-o>w")
+
+-- delete words
+vim.keymap.set("i", "<M-Del>", "<C-o>dw")
+vim.keymap.set("i", "<M-BS>", "<C-o>db")
 
 -- just to be able to swap selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -27,5 +40,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>ss", [[/\<<C-r><C-w>\>]])
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
